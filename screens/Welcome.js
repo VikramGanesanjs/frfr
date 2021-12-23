f9cdfb50e378eb9b62c81105c1de7292318e66c9
vkganesan666 <vkganesan666@gmail.com>
2021-12-23 11:49:41 -0800

    Added retrieve name to Welcome screen

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -27,14 +27,38 @@ import News from '../tabBarIcons/News';
 import Account from '../tabBarIcons/Account';
 import Trending from '../tabBarIcons/Trending';
 import Search from '../tabBarIcons/Search';
-import { auth } from '../config/firebase';
 import { signOut } from 'firebase/auth';
+import { doc, getDoc } from "firebase/firestore";
+import { db, auth } from '../config/firebase';
+
+
+import { useState, useEffect } from 'react';
 
 // Colors
 const{brand, darkLight, primary} = Colors;
 
 
+
 const Welcome = ({ navigation }) => {
+    
+    const [name, setName] = useState('Welcome Dude')
+
+    const retrieveName = async () => {
+        
+        const docRef = doc(db, "Users", auth.currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        if (typeof docSnap !== undefined) {
+            const fullName = (docSnap._document.data.value.mapValue.fields.fullName);
+            setName(`Welcome ${fullName.stringValue}!`);
+        } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        }
+    }
+
+    useEffect(() => {
+        retrieveName();
+    })
     
 
     return(
@@ -44,7 +68,7 @@ const Welcome = ({ navigation }) => {
                 
                 <WelcomeContainer>
 
-                    <PageTitle welcome={true}>Welcome! Buddy</PageTitle>
+                    <PageTitle welcome={true}>{name}</PageTitle>
                     <SubTitle welcome={true}>Olga Simpson</SubTitle>
                     <SubTitle welcome={true}>johndoe@email.com</SubTitle>
 
