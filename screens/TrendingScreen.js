0e39b2bd3014df3d8c80be500f22b71d5d57f305
vkganesan666 <vkganesan666@gmail.com>
2022-01-21 17:38:41 -0800

    removed unneccesary packages

--- screens/TrendingScreen.js
+++ screens/TrendingScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }from 'react';
+import React, { useEffect, useState } from 'react';
 import {View, Text, TouchableOpacity, SafeAreaView} from 'react-native';
 
 
@@ -24,16 +24,14 @@ import{
 
 const {primary, secondary, tertiary, brand, white} = Colors;
 
-import {Calendar, Agenda} from "react-native-calendars";
+import {Agenda} from "react-native-calendars";
 import { isEqual } from 'lodash';
 
 import { db, auth } from '../config/firebase';
 import { collection, getDocs, doc, Timestamp } from 'firebase/firestore';
-import { format } from 'date-fns/esm';
 
 const TrendingScreen = () => {
 
-    const [numberOfDays, setNumberOfDays] = useState(31)
     const [currentDate, setCurrentDate] = useState('');
     const [minDate, setMinDate] = useState('');
     const [maxDate, setMaxDate] = useState('');
@@ -41,6 +39,12 @@ const TrendingScreen = () => {
     const [eventData, setEventData] = useState([]);
     const [items, setItems] = useState({})
     const [markedItems, setMarkedItems] = useState({});
+
+
+    
+    var width = Dimensions.get('window').width; //full width
+    var height = Dimensions.get('window').height; 
+
 
 
 
@@ -154,86 +158,112 @@ const TrendingScreen = () => {
         <SafeAreaView style={{
             backgroundColor: primary,
         }}>
-        <CalendarContainer>
+          <CalendarContainer>
             
-                <Agenda
-  // The list of items that have to be displayed in agenda. If you want to render item as empty date
-  // the value of date key has to be an empty array []. If there exists no value for date key it is
-  // considered that the date in question is not yet loaded
-  items={items}
-  // Callback that gets called when items for a certain month should be loaded (month became visible)
-  // Callback that gets called when day changes while scrolling agenda list
-  onDayChange={day => {
-    console.log('day changed');
-  }}
-  // Initially selected day
-  current={currentDate}
-  // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-  minDate={minDate}
-  // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-  maxDate={maxDate}
-  // Max amount of months allowed to scroll to the past. Default = 50
-  pastScrollRange={50}
-  // Max amount of months allowed to scroll to the future. Default = 50
-  futureScrollRange={50}
-  // Specify how each item should be rendered in agenda
-  renderItem={(item, firstItemInDay) => {
-    return (<View>
-      <Text>
-        {item.name ? item.name : Hello}
-      </Text>
-    </View>);
-  }}
-  // Specify how each date should be rendered. day can be undefined if the item is not first in that day
-  renderDay={(day, item) => {
-    return <View/>
-     
-  }}
-  // Specify how empty date content with no items should be rendered
-  renderEmptyDate={() => {
-    return <View />;
-  }}
-  // Specify how agenda knob should look like
-  // Specify what should be rendered instead of ActivityIndicator
-  renderEmptyData={() => {
-    return <View />;
-  }}
-  // Specify your item comparison function for increased performance
-  rowHasChanged={(r1, r2) => {
-    return r1.text !== r2.text;
-  }}
-  // Hide knob button. Default = false
+            <Agenda
+              // The list of items that have to be displayed in agenda. If you want to render item as empty date
+              // the value of date key has to be an empty array []. If there exists no value for date key it is
+              // considered that the date in question is not yet loaded
+              items={items}
+              // Callback that gets called when items for a certain month should be loaded (month became visible)
+              // Callback that gets called when day changes while scrolling agenda list
+              onDayChange={day => {
+                console.log('day changed');
+              }}
+              // Initially selected day
+              current={currentDate}
+              // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+              minDate={minDate}
+              // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+              maxDate={maxDate}
+              // Max amount of months allowed to scroll to the past. Default = 50
+              pastScrollRange={50}
+              // Max amount of months allowed to scroll to the future. Default = 50
+              futureScrollRange={50}
+              // Specify how each item should be rendered in agenda
+              renderItem={(item, firstItemInDay) => {
+                return (<View>
+                  <Text>
+                    {item.name ? item.name : Hello}
+                  </Text>
+                </View>);
+              }}
+              // Specify how each date should be rendered. day can be undefined if the item is not first in that day
+              renderDay={(day, item) => {
+                return(
+                  <View style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    height: 500,
+                    width: width,
+                    backgroundColor: white
+                  }}>
+                    <View style={{
+                      display: 'flex',
+                      height: 500,
+                      width: 20,
+                      backgroundColor: tertiary
+                    }}>
 
-  // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-  markedDates={markedItems}
-  // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
-  disabledByDefault={true}
-  // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly
-  onRefresh={() => console.log('refreshing...')}
-  // Set this true while waiting for new data from a refresh
-  refreshing={false}
-  // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView
-  refreshControl={null}
-  // Agenda theme
-  theme={{
-    backgroundColor: primary,
-    calendarBackground: primary,
-    monthTextColor: brand,
-    
-    agendaDayTextColor: 'yellow',
-    agendaDayNumColor: 'green',
-    agendaTodayColor: 'red',
-    agendaKnobColor: brand,
-  }}
-  // Agenda container style
-  style={{
-    backgroundColor: primary,
-   
-  }}
-/>
+                    </View>
+                    <View style={{
+                      display: 'flex',
+                      height: 500,
+                      width: width - 20,
+                      backgroundColor: tertiary
+                    }}>
+                      {item.name ? item.name : Hello}
 
-</CalendarContainer>
-</SafeAreaView>
+                    </View>
+                  </View>
+                );
+                
+              }}
+              // Specify how empty date content with no items should be rendered
+              renderEmptyDate={() => {
+                return <View />;
+              }}
+              // Specify how agenda knob should look like
+              // Specify what should be rendered instead of ActivityIndicator
+              renderEmptyData={() => {
+                return <View />;
+              }}
+              // Specify your item comparison function for increased performance
+              rowHasChanged={(r1, r2) => {
+                return r1.text !== r2.text;
+              }}
+              // Hide knob button. Default = false
+
+              // By default, agenda dates are marked if they have at least one item, but you can override this if needed
+              markedDates={markedItems}
+              // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
+              disabledByDefault={true}
+              // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly
+              onRefresh={() => console.log('refreshing...')}
+              // Set this true while waiting for new data from a refresh
+              refreshing={false}
+              // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView
+              refreshControl={null}
+              // Agenda theme
+              theme={{
+                backgroundColor: primary,
+                calendarBackground: primary,
+                monthTextColor: brand,
+                
+                agendaDayTextColor: 'yellow',
+                agendaDayNumColor: 'green',
+                agendaTodayColor: 'red',
+                agendaKnobColor: brand,
+              }}
+              // Agenda container style
+              style={{
+                backgroundColor: primary,
+              
+              }}
+            />
+
+        </CalendarContainer>
+      </SafeAreaView>
     
     );
 
